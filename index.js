6374c4851a1f3aa141d4f21b398ce48ef8a73f1f
Mohsin <mohsin-ahmed01@outlook.com>
2023-09-07 17:46:09 +0100

    refactored code to use functions & new line is placed after each completed calculation

--- index.js
+++ index.js
@@ -5,38 +5,53 @@ function printWelcomeMessage () {
     console.log("==========================");
 };
 
-printWelcomeMessage();
-
-console.log("Please enter the operator: ");
-const operator = readline.prompt(); 
-
-console.log(`How many numbers do you want to ${operator} together: `);
-const numbersrequired = Number(readline.prompt());
-
-
-let response;
-const arr = [];
-for (let i = 1; i < numbersrequired + 1; i++) {
-    console.log(`Please enter number ${i}: `);
-    response = Number(readline.prompt());
-    arr.push(response);
+function getInputWithPrompt (prompt) {
+    console.log(prompt);
+    return readline.prompt();
 };
 
-let answer = arr[0];
-for (let j = 0; j < arr.length - 1; j++) {
+function getOperator () {
+    return getInputWithPrompt("\nPlease enter the operator: ") 
+};
+
+function getNumbersArray(operator) {
+    const numbersRequired = Number(getInputWithPrompt(`How many numbers do you want to ${operator} together: `)); 
+    
+    const arr = [];
+    for (let i = 1; i < numbersRequired + 1; i++) {
+    arr.push(Number(getInputWithPrompt(`Please enter number ${i}: `)));
+};
+return arr;
+};
+
+function calculateAnswer(operator, array) {
+    let answer = array[0];
+    for (let j = 0; j < array.length - 1; j++) {
     if (operator == '+') {
-        answer = answer + arr[j + 1];
+        answer = answer + array[j + 1];
     } else if (operator == '-') {
-        answer = answer + arr[j + 1];
+        answer = answer + array[j + 1];
     } else if (operator == '*') {
-        answer = answer + arr[j + 1];
+        answer = answer + array[j + 1];
     } else if (operator == '/') {
-        answer = answer + arr[j + 1];
+        answer = answer + array[j + 1];
     } else {
         console.log("Invalid operator");
         answer = 'Error';
         break;
     };
 };
+return answer;
+}
 
-console.log(`The answer is ${answer}!`);
+function performOneCalculation () {
+    const chosenOperator = getOperator();
+    let numbersArray = getNumbersArray(chosenOperator); // check if it works with let or const, unsure if function call assignment and function return will work on const
+    const finalAnswer = calculateAnswer(chosenOperator, numbersArray)
+    console.log(`The answer is ${finalAnswer}!`);
+};
+
+printWelcomeMessage();
+while (true) {
+    performOneCalculation();
+};
