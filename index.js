230892567886b9ccc6861b4cf29a37ab57c6560d
Mohsin <mohsin-ahmed01@outlook.com>
2023-09-07 21:54:11 +0100

    added more functionality

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ function getInputWithPrompt (prompt) {
     return readline.prompt();
 };
 
+function getCalculationMode () {
+    return getInputWithPrompt(`
+    Which calculator mode do you want?
+    Enter 1 for Arithmetic
+    Enter 2 for Vowel Counting`
+    )
+}
+
 function getOperator () {
     return getInputWithPrompt("\nPlease enter the operator: ") 
 };
+
+function getString () {
+    return getInputWithPrompt("\nPlease enter the string: ").toUpperCase();
+    
+}
 
 function getNumbersArray(operator) {
     const numbersRequired = Number(getInputWithPrompt(`How many numbers do you want to ${operator} together: `)); 
@@ -44,14 +57,70 @@ function calculateAnswer(operator, array) {
 return answer;
 }
 
-function performOneCalculation () {
+function calculateVowels (string) {
+  // use an object to hold the answer while doing the vowel counting calculation
+  // add support for counting the number of times each vowel appears in a given string
+
+  const vowels = {
+    A: 0,
+    E: 0,
+    I: 0,
+    O: 0,
+    U: 0,
+  }
+
+  for (letter in string) {
+    if (letter === 'A') {
+        vowels.A = vowels.A + 1
+    } else if (letter === 'E') {
+        vowels.E = vowels.E + 1
+    } else if (letter === 'I') {
+        vowels.I = vowels.I + 1
+    } else if (letter === 'O') {
+        vowels.O = vowels.O + 1
+    } else if (letter === 'U') {
+        vowels.U = vowels.U + 1
+    }
+  }
+
+  // return the object
+  return vowels;
+};
+
+function performOneArithmeticCalculation () {
     const chosenOperator = getOperator();
     const numbersArray = getNumbersArray(chosenOperator);
     const finalAnswer = calculateAnswer(chosenOperator, numbersArray)
     console.log(`The answer is ${finalAnswer}!`);
 };
 
+function performOneVowelCountingCalculation () {
+    const givenString = getString();
+
+    // call the functuon and assign the object to a variable
+    const calculatedVowelsObject = calculateVowels(givenString);
+    
+    // output the "the vowel counts are" text and the object as shown in the pdf
+    console.log(`The vowel counts are:
+    ${calculatedVowelsObject}
+    `);
+};
+
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
+
 printWelcomeMessage();
 while (true) {
-    performOneCalculation();
+    const calculationMode = getCalculationMode();
+    if (calculationMode === ARITHMETIC_MODE) {
+    performOneArithmeticCalculation();
+    } else if (calculationMode === VOWEL_COUNTING_MODE) {
+    performOneVowelCountingCalculation();
+    }
+    
 };
+
+
+
+
+
